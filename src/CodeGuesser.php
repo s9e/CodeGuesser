0e2c023f448ded5125961bf5f372b6101cc83b5c
JoshyPHP <s9e.dev@gmail.com>
2022-09-24 14:12:37 +0200

    Initial commit

---
+++ src/CodeGuesser.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+/**
+* @package   s9e\CodeGuesser
+* @copyright Copyright (c) 2022 The s9e authors
+* @license   https://opensource.org/licenses/mit-license.php The MIT License
+*/
+namespace s9e\CodeGuesser;
+
+class CodeGuesser
+{
+}
